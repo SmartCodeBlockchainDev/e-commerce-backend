5e26152c41ab34172f273b438833ec85552f7fca
maui <maui@devf.mx>
2020-11-23 17:00:40 -0600

    fix: Config file

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ const env = process.env.NODE_ENV || 'development'; // 'dev' or 'test' or 'produc
 const SECRET_KEY = process.env.SECRET_KEY || 'clavesecretaparajwt';
 
 const development = {
-  mongoURI: `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.quach.mongodb.net/witlabdev?retryWrites=true&w=majority`,
+  mongoURI: `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.quach.mongodb.net/development?retryWrites=true&w=majority`,
   SECRET_KEY,
 };
 
 const production = {
-  mongoURI: `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.quach.mongodb.net/witlab?retryWrites=true&w=majority`,
+  mongoURI: `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.quach.mongodb.net/production?retryWrites=true&w=majority`,
   SECRET_KEY,
 };
 
