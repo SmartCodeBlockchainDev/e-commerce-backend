519ff5d3a2f8b0b67360489ed331d357beb68eb7
Ligorio Edwin Salgado Flores <edwinsalgado@Mac-mini-de-Ligorio.local>
2020-11-23 17:03:50 -0600

    add items

---
+++ services/ItemService.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { Item } = require('../models');
+const config = require('../config');
+const BackendError = require('../errors/BackendError');
+
+module.exports = {
+  
+    allItems: () => Item.find({})
+                    .then(items => items)
+                    .catch(err => { throw new BackendError(err); }),
+    getItemBySKU: (sku)  => Item.findOne({sku})
+                        .then(item => item)
+                        .catch(err => { throw new BackendError(err); }),
+    getItemsByCategory: category => Item.findOne({category})
+                                        .then(items =>items)
+                                        .catch(err => { throw new BackendError(err); }),
+    getItemByProductName: name => Item.find({'product_name':{'$regex':`%${name}%`}})
+                                        .then(items =>items)
+                                        .catch(err => { throw new BackendError(err); }),
+    getItemById: _id => Item.findOne(_id)
+                            .then(item => item)
+                            .catch(err => { throw new BackendError(err); }),
+    queryGetItems: query => Item.find({...query})
+                                .then(items =>items)
+                                .catch(err => { throw new BackendError(err); }),
+                                    
+    
+
+};
--- utils/const.js
+++ utils/const.js
@@ -1,5 +1,29 @@
 module.exports = {
     ENUM_GENDER: ['M', 'F', 'X'],
     ENUM_ROLE: ['ADMIN', 'CUSTOMER'],
+    ENUM_CATEGORY: [
+      "Books",
+      "Movies",
+      "Music",
+      "Games",
+      "Electronics",
+      "Computers",
+      "Home",
+      "Garden",
+      "Tools",
+      "Grocery",
+      "Health",
+      "Beauty",
+      "Toys",
+      "Kids",
+      "Baby",
+      "Clothing",
+      "Shoes",
+      "Jewelery",
+      "Sports",
+      "Outdoors",
+      "Automotive",
+      "Industrial"
+    ]
   };
   
