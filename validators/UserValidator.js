9efa9c0ad65e7378628d1073b8cb9656b0e15abd
maui <maui@devf.mx>
2020-11-23 18:14:04 -0600

    feat: Add update user validator

--- validators/UserValidator.js
+++ validators/UserValidator.js
@@ -1,11 +1,15 @@
 const { celebrate, Joi } = require('celebrate');
+const { ENUM_GENDER } = require('../utils/const');
 
 module.exports = {
   update: celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required(),
-      age: Joi.number().integer(),
-      role: Joi.string().default('admin')
+      first_name: Joi.string(),
+      last_name: Joi.string(),
+      email: Joi.string(),
+      password: Joi.string(),
+      birth_date: Joi.date(),
+      gender: Joi.string().valid(ENUM_GENDER),
     }),
   }),
 }
