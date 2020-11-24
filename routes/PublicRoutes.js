06f72defebe51b3be723b8d298420aa128c14333
maui <maui@devf.mx>
2020-11-23 18:17:42 -0600

    fix: Item endpoints

--- routes/PublicRoutes.js
+++ routes/PublicRoutes.js
@@ -8,8 +8,9 @@ const router = express.Router();
 // Auth
 router.use(require('./AuthRoutes'));
 
-router.get('/items', [], ItemController.fetchItems(ItemService, asyncError))
-router.get('/items', [], ItemController.retriveItem(ItemService, asyncError))
+// Items
+router.get('/item', ItemController.fetchItems(ItemService, asyncError))
+router.get('/item/:idItem', ItemController.retriveItem(ItemService, asyncError))
 
 
 module.exports = router;
